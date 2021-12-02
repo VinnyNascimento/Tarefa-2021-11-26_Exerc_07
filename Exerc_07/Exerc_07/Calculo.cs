7469767e4f51221f27b14c22988ba39ff0642359
VinnyNascimento <91098218+VinnyNascimento@users.noreply.github.com>
2021-12-02 15:12:33 -0300

    Add files via upload

---
+++ Exerc_07/Exerc_07/Calculo.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exerc_07
+{
+    class Calculo
+    {
+        #region atributos
+        private double num1;
+        private double num2;
+        private double resultado;
+        #endregion
+
+        #region construtores
+        public Calculo()
+        {
+            this.num1 = 0;
+            this.num2 = 0;
+        }
+
+        public Calculo(double num1, double num2)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+        }
+        #endregion
+
+        #region métodos de interface (Setter's & Getter's)
+        public void setNum1(double num1)
+        {
+            this.num1 = num1;
+        }
+
+        public void setNum2(double num2)
+        {
+            this.num2 = num2;
+        }
+
+        public double getNum1()
+        {
+            return this.num1;
+        }
+
+        public double getNum2()
+        {
+            return this.num2;
+        }
+
+        public double getResultado()
+        {
+            return this.resultado;
+        }
+        #endregion
+
+        #region métodos funcionais
+        public void calcular()
+        {
+            this.resultado = this.num1 * this.num2;
+        }
+        #endregion
+    }
+}
---
+++ Exerc_07/Exerc_07/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿
+namespace Exerc_07
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.txtCotacao = new System.Windows.Forms.TextBox();
+            this.txtDolar = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(162, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(328, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Faça a cotação do dólar em Reais (R$)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(48, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(229, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Valor da cotação do dólar";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(411, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(194, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Valor em dólares (U$)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(100, 278);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(274, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Cotação do dólar em Reais (R$)";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblResultado.Location = new System.Drawing.Point(437, 278);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(52, 25);
+            this.lblResultado.TabIndex = 4;
+            this.lblResultado.Text = "        ";
+            // 
+            // txtCotacao
+            // 
+            this.txtCotacao.Location = new System.Drawing.Point(117, 144);
+            this.txtCotacao.Name = "txtCotacao";
+            this.txtCotacao.Size = new System.Drawing.Size(100, 23);
+            this.txtCotacao.TabIndex = 5;
+            // 
+            // txtDolar
+            // 
+            this.txtDolar.Location = new System.Drawing.Point(458, 144);
+            this.txtDolar.Name = "txtDolar";
+            this.txtDolar.Size = new System.Drawing.Size(100, 23);
+            this.txtDolar.TabIndex = 6;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnCalcular.Location = new System.Drawing.Point(292, 192);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(98, 52);
+            this.btnCalcular.TabIndex = 7;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 345);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtDolar);
+            this.Controls.Add(this.txtCotacao);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox txtCotacao;
+        private System.Windows.Forms.TextBox txtDolar;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
